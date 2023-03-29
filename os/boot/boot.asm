48e7d62156d4a8eda0fc0b7e73e6ca9c4ddfe668
suleyman <suleymankayaxyz@gmail.com>
2023-03-29 14:50:53 +0300

    graphics mode unlocked

--- os/boot/boot.asm
+++ os/boot/boot.asm
@@ -4,9 +4,25 @@
 section code
 
 .switch:
+    mov ax, 0x4f01 ; querying the VBE
+    mov cx, 0x117 ; Mode we want
+    mov bx, 0x0800 ; Offset for the vbe info structure
+    mov es, bx
+    mov di, 0x00
+    int 0x10
+
+    ; Make the switch to graphics mode
+    mov ax, 0x4f02
+    mov bx, 0x117
+    int 0x10
+
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+
     mov bx, 0x1000 ; This is the location where the code is loaded from hard disk
     mov ah, 0x02
-    mov al, 30 ; The number of sectors to read from hard disk
+    mov al, 1 ; The number of sectors to read from hard disk
     mov ch, 0x00
     mov dh, 0x00
     mov cl, 0x02
