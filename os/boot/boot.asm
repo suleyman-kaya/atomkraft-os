7c63d5f228c702740cb9f8e62d5d097c582555b2
suleyman <suleymankayaxyz@gmail.com>
2023-03-29 12:47:33 +0300

    added print utility

--- os/boot/boot.asm
+++ os/boot/boot.asm
@@ -4,27 +4,52 @@
 section code
 
 .init:
+    mov eax, 0x07c0
+    mov ds, eax
     mov eax, 0xb800
     mov es, eax
-    mov eax, 0 ; set eax to 0, kind of i = 0 in for loops
+    mov eax, 0 ; set eax to 0 -> i = 0
+    mov ebx, 0 ; Index of the character in the string that we are printing
+    mov ecx, 0 ; Actual address of the chracter on the screen memory
+    mov dl, 0 ; Actual value of the character that we are printing
 
 .clear:
-    mov byte [es:eax], 0 ; Move blank character to current text address.
+    mov byte [es:eax], 0
     inc eax
-    mov byte [es:eax], 0x30 ; Move the background colour and character colour to the next address
+    mov byte [es:eax], 0x30
     inc eax
 
-    cmp eax, 2*25*80
+    cmp eax, 2 * 25 * 80
 
     jl .clear
 
-.main:
-    mov byte [es:0x00], 'H' ; 0xb800 + 0x00 = 0xb800
-    mov byte [es:0x01], 0x67
+mov eax, text1
+call .print
 
-jmp $
+.end:
+    jmp $
 
-times 510 - ($ - $$) db 0x00 ; pads the file with zeros, making it right size.
+.print:
+    mov dl, byte [eax + ebx]
+    
+    cmp dl, 0
+    je .print_end
+    
+    mov byte [es:ecx], dl
+
+    inc ebx
+    inc ecx
+    inc ecx
+    
+    jmp .print
+
+.print_end:
+    ret
+
+text: db 'Hello world!', 0
+text1: db 'This is my first operating system programming experience!', 0
+
+times 510 - ($ - $$) db 0x00 ; Pads the file with 0s, making the file the right size
 
 db 0x55
 db 0xaa
